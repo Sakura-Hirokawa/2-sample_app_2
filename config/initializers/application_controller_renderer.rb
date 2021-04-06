028caadfaa2394a73410697808e39e2f3d7a5f33
EC2 Default User <sk.hirokawa@gmail.com>
2021-04-06 09:37:14 +0000

    [Finish]2-7/attachment

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a702b3ba962fe92214afbdd77145bee186d245138952ed9e3f010bdb78e3c40d62e8a9847a010c6b77b64b9defaeaaa7fad2799167eb970b6c433faab55aa27e'
